ee710fd34c4fc49419f484e69f65751819dfb920
jonadsimon <jonathan.simon706@gmail.com>
2019-01-04 16:08:53 -0800

    two column outputs

--- app/routes.py
+++ app/routes.py
@@ -4,25 +4,31 @@ from app.models import Word
 from app.forms import InputWords
 from app.helper_utils import get_semantic_neighbor_graphemes, get_portmanteaus, get_rhymes
 from app.global_constants import MAX_PORTMANTEAUS, MAX_RHYMES
-import pdb
+from time import time
 
 def get_puns_from_form_data(form):
     # Find the semantic neighbors of the graphemes
+    start = time()
     nearest_graphemes1 = get_semantic_neighbor_graphemes(form.word1.data)
     nearest_graphemes2 = get_semantic_neighbor_graphemes(form.word2.data)
+    print "Semantic neighbors: {:.2f} seconds".format(time()-start)
 
     # Find the Word objects corresponding to each of the semantic neighbors
+    start = time()
     nearest_words1 = Word.query.filter(Word.grapheme.in_(nearest_graphemes1)).all()
     nearest_words2 = Word.query.filter(Word.grapheme.in_(nearest_graphemes2)).all()
+    print "Word conversion: {:.2f} seconds".format(time()-start)
 
     # Generate the ordered portmanteaus and rhymes
+    start = time()
     portmanteaus = get_portmanteaus(nearest_words1, nearest_words2)
     rhymes = get_rhymes(nearest_words1, nearest_words2)
+    print "Portmanteaus & Rhymes: {:.2f} seconds".format(time()-start)
 
     return {'portmanteaus': map(lambda x: x.__str__(), portmanteaus[:MAX_PORTMANTEAUS]), 'rhymes': map(lambda x: x.__str__(), rhymes[:MAX_RHYMES])}
 
 @app.route('/')
-@app.route('/generate_puns', methods=['GET', 'POST'])
+@app.route('/generate_puns', methods=['GET', 'POST']) # TODO : remove this in favor of base url
 def generate_puns():
     '''
     Display the results, and continue prompting for input
