b6f3ad56d72556890e7e9e66424695c363a0bbba
jonadsimon <jonathan.simon706@gmail.com>
2019-02-16 20:16:40 -0800

    added Talisman for https redirects

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_talisman import Talisman
 from flask_bootstrap import Bootstrap
 from flask_heroku import Heroku
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+Talisman(app)
 Bootstrap(app)
 heroku = Heroku(app)
 db = SQLAlchemy(app)
